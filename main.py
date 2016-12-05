fc0005fb38170ed67b300cfb4953319523a94bd6
Kirill Erimeev <websprogrammer@gmail.com>
2016-12-05 14:04:15 +0300

    Small refactor

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
+import os
 import unittest
 from selenium import webdriver
-from time import sleep
-import os
 
 
 class PythonMarketSearch(unittest.TestCase):
+    """We make 2 tests for yandex.market products"""
+
     def setUp(self):
-        self.driver = webdriver.Chrome('%s\chromedriver' % os.getcwd())
+        """Initialization of driver."""
+        self.driver = webdriver.Chrome('{}\chromedriver'.format(os.getcwd()))
         self.driver.maximize_window()
         self.driver.get('https://yandex.ru')
 
     def click_to_hidden(self, title):
+        """Control of click to hidden element"""
         element = self.driver.find_element_by_xpath('//*[@class="topmenu__subwrap"]')
         self.driver.execute_script('arguments[0].style.display="block"', element)
-        self.driver.find_element_by_xpath('//a[contains(text(), "%s")]' % title).click()
-        sleep(2)
+        self.driver.find_element_by_xpath('//a[contains(text(), "{}")]'.format(title)).click()
 
     def submit_form(self, inputs, checkboxes, btn):
-
+        """Submit form with passed parameters"""
         for key, value in inputs.items():
-            input = self.driver.find_element_by_xpath(key)
-            input.send_keys(value)
+            input_ = self.driver.find_element_by_xpath(key)
+            input_.send_keys(value)
 
         for checkbox in checkboxes:
             self.driver.find_element_by_xpath(checkbox).click()
 
         self.driver.find_element_by_xpath(btn).click()
-        sleep(2)
 
     def make_click(self, xpath):
+        """Control of click to element by xpath"""
+
         self.driver.find_element_by_xpath(xpath).click()
-        sleep(2)
 
     def test_search_in_yandex_market(self):
+        """
+        Test case 1: we go to Market page and try to get information
+        about TVs filtered by price
+        """
         driver = self.driver
 
         self.make_click('//a[@data-id="market"]')
@@ -47,7 +53,9 @@ class PythonMarketSearch(unittest.TestCase):
             '//button[contains(./span/text(), "Применить")]'
         )
 
-        goods = driver.find_elements_by_xpath('//div[contains(@class, "snippet-card ") and @data-id]//h3')
+        goods = driver.find_elements_by_xpath(
+            '//div[contains(@class, "snippet-card ") and @data-id]//h3'
+        )
 
         if len(goods) == 10:
             first_product = goods[0].text
@@ -62,6 +70,11 @@ class PythonMarketSearch(unittest.TestCase):
             self.assertEqual(first_product, title)
 
     def test_search_in_yandex_market2(self):
+        """
+        Test case 1: we go to Market page and try to get information
+        about headsets filtered by price
+        """
+
         driver = self.driver
 
         self.make_click('//a[@data-id="market"]')
@@ -75,7 +88,9 @@ class PythonMarketSearch(unittest.TestCase):
             '//button[contains(./span/text(), "Применить")]'
         )
 
-        goods = driver.find_elements_by_xpath('//div[contains(@class, "snippet-card ") and @data-id]//h3')
+        goods = driver.find_elements_by_xpath(
+            '//div[contains(@class, "snippet-card ") and @data-id]//h3'
+        )
 
         if len(goods) == 10:
             first_product = goods[0].text
